cd279c61b95b0dbbfc6a824d4c6651b70257d431
Milly Flores <miflo0h@gmail.com>
2020-05-11 18:56:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,6 @@
-Join Zoom Meeting
-https://us04web.zoom.us/j/9611628292?pwd=cWZvNFFDdDd1MFN3ZUtGdGtZREU4QT09
-
-Meeting ID: 961 162 8292
-Password: 2VJi2P
-
-
-
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+def introduction_with_language (name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
